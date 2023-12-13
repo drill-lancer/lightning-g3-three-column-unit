b5efbf375f4c7d34cb229e6113bd53b647429d84
drill-lancer <rickaddison7634@gmail.com>
2023-12-13 11:31:27 +0900

    fix: custmize

--- modules/class-admin.php
+++ modules/class-admin.php
@@ -17,6 +17,7 @@ class Admin {
 	 */
 	public function __construct() {
 		add_action( 'customize_register', array( __CLASS__, 'resister_customize' ), 11 );
+		add_action( 'customize_controls_enqueue_scripts', array( __CLASS__, 'custmoze_script' ) );
 	}
 
 	/**
@@ -38,6 +39,85 @@ class Admin {
 	}
 
 	/**
+	 * Custmoze Script
+	 */
+	public static function custmoze_script() {
+		$options = get_option( 'lightning_g3_three_column_unit_options' );
+		$script = <<<EOT
+		const lightning_g3_three_column_unit_options = {
+			main_width: {$options['main_width']},
+			side_width: {$options['side_width']},
+			column_margin: {$options['column_margin']},
+			outer_container_margin: {$options['outer_container_margin']},
+			three_to_one_via_two: '{$options['three-to-one-via-two']}',
+			main_sidebar_control: '{$options['main_sidebar_control']}',
+			sub_sidebar_control: '{$options['sub_sidebar_control']}',
+			2col_sub_sidebar_position: '{$options['2col_sub_sidebar_position']}',
+			narrow_window_description: '{$options['narrow_window_description']}',
+		};
+		wp.customize.bind( 'ready', function() {
+			wp.customize( 'lightning_g3_three_column_unit_options[main_width]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.main_width = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[side_width]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.side_width = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[column_margin]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.column_margin = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[outer_container_margin]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.outer_container_margin = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[three-to-one-via-two]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.three_to_one_via_two = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[main_sidebar_control]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.main_sidebar_control = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[sub_sidebar_control]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.sub_sidebar_control = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options.2col_sub_sidebar_position = to;
+				} );
+			} );
+			wp.customize( 'lightning_g3_three_column_unit_options[narrow_window_description]', function( value ) {
+				value.bind( function( to ) {
+					lightning_g3_three_column_unit_options,narrow_window_description = to;
+				} );
+			} );
+		} );
+
+		wp.customize.bind( 'change', function() {
+			if ( lightning_g3_three_column_unit_options.three_to_one_via_two === 'enable' ) {
+				if ( lightning_g3_three_column_unit_options.sub_sidebar_control === 'wrap-down' ) {
+					wp.customize.control( 'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]' ).activate();
+				} else {
+					wp.customize.control( 'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]' ).deactivate();
+				}
+			}
+		} );
+		EOT;
+
+		wp_add_inline_script( 'customize-controls', $script );
+	}
+
+	/**
 	 * Register Customize
 	 */
 	public static function resister_customize() {
@@ -45,6 +125,8 @@ class Admin {
 		global $wp_customize;
 
 		$default_option = self::default_option();
+		$current_option = get_option( 'lightning_g3_three_column_unit_options' );
+		require_once plugin_dir_path( __FILE__ ) . 'class-vk-custom-html-control.php';
 
 		// Add Section.
 		$wp_customize->add_section(
@@ -60,6 +142,27 @@ class Admin {
 		$wp_customize->remove_control( 'ltg_sidebar_fix_setting_title' );
 		$wp_customize->remove_setting( 'lightning_theme_options[sidebar_fix]' );
 		$wp_customize->remove_control( 'lightning_theme_options[sidebar_fix]' );
+
+
+		$wp_customize->add_setting(
+			'width-setting',
+			array(
+				'sanitize_callback' => 'sanitize_text_field',
+			)
+		);
+		$wp_customize->add_control(
+			new \VK_Custom_Html_Control(
+				$wp_customize,
+				'width-setting',
+				array(
+					'label'            => __( 'Width Setting', 'lightning-g3-three-column-unit' ),
+					'section'          => 'lightning_g3_three_column_unit_setting',
+					'type'             => 'text',
+					'custom_title_sub' => '',
+					'custom_html'      => '',
+				)
+			)
+		);
 
 		// Main Culumn Width.
 		$wp_customize->add_setting(
@@ -141,30 +244,26 @@ class Admin {
 			)
 		);
 
-		// When Three Column Layout is Choosed and Narrowing Window Size, Make Two Column Layout.
 		$wp_customize->add_setting(
-			'lightning_g3_three_column_unit_options[three-to-one-via-two]',
+			'sidebar-setting',
 			array(
-				'default'           => $default_option['three-to-one-via-two'],
-				'type'              => 'option',
-				'capability'        => 'edit_theme_options',
 				'sanitize_callback' => 'sanitize_text_field',
 			)
 		);
 		$wp_customize->add_control(
-			'lightning_g3_three_column_unit_options[three-to-one-via-two]',
-			array(
-				'label'    => __( 'When Three Column Layout is Choosed and Narrowing Window Size, Make Two Column Layout', 'lightning-g3-three-column-unit' ),
-				'section'  => 'lightning_g3_three_column_unit_setting',
-				'settings' => 'lightning_g3_three_column_unit_options[three-to-one-via-two]',
-				'type'     => 'select',
-				'choices'  => array(
-					'disable' => __( 'Disable', 'lightning-g3-three-column-unit' ),
-					'enable'  => __( 'Enable', 'lightning-g3-three-column-unit' ),
-				),
+			new \VK_Custom_Html_Control(
+				$wp_customize,
+				'sidebar-setting',
+				array(
+					'label'            => __( 'Sidebar Setting', 'lightning-g3-three-column-unit' ),
+					'section'          => 'lightning_g3_three_column_unit_setting',
+					'type'             => 'text',
+					'custom_title_sub' => '',
+					'custom_html'      => '',
+				)
 			)
 		);
-
+		
 		// Main Sidebar Control.
 		$wp_customize->add_setting(
 			'lightning_g3_three_column_unit_options[main_sidebar_control]',
@@ -188,6 +287,7 @@ class Admin {
 				),
 			)
 		);
+		
 
 		// Sub Sidebar Control.
 		$wp_customize->add_setting(
@@ -213,6 +313,51 @@ class Admin {
 			)
 		);
 
+		$wp_customize->add_setting(
+			'three-column-setting',
+			array(
+				'sanitize_callback' => 'sanitize_text_field',
+			)
+		);
+		$wp_customize->add_control(
+			new \VK_Custom_Html_Control(
+				$wp_customize,
+				'three-column-setting',
+				array(
+					'label'            => __( 'Three Column Layout Setting', 'lightning-g3-three-column-unit' ),
+					'section'          => 'lightning_g3_three_column_unit_setting',
+					'type'             => 'text',
+					'custom_title_sub' => '',
+					'custom_html'      => '',
+				)
+			)
+		);
+
+
+		$wp_customize->add_setting(
+			'lightning_g3_three_column_unit_options[three-to-one-via-two]',
+			array(
+				'default'           => $default_option['three-to-one-via-two'],
+				'type'              => 'option',
+				'capability'        => 'edit_theme_options',
+				'sanitize_callback' => 'sanitize_text_field',
+			)
+		);
+		$wp_customize->add_control(
+			'lightning_g3_three_column_unit_options[three-to-one-via-two]',
+			array(
+				'label'    => __( 'Make Two Column Layout on Marrow Window', 'lightning-g3-three-column-unit' ),
+				'section'  => 'lightning_g3_three_column_unit_setting',
+				'settings' => 'lightning_g3_three_column_unit_options[three-to-one-via-two]',
+				'type'     => 'select',
+				'choices'  => array(
+					'disable' => __( 'Disable', 'lightning-g3-three-column-unit' ),
+					'enable'  => __( 'Enable', 'lightning-g3-three-column-unit' ),
+				),
+			)
+		);
+
+
 		// Sub Sidebar Control.
 		$wp_customize->add_setting(
 			'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]',
@@ -226,7 +371,7 @@ class Admin {
 		$wp_customize->add_control(
 			'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]',
 			array(
-				'label'    => __( 'Sub Sidebar Position When Selected Layout is 3 Column Layout and Current Layout is 2 Column Layout', 'lightning-g3-three-column-unit' ),
+				'label'    => __( 'Wrapping position of sub sidebar', 'lightning-g3-three-column-unit' ),
 				'section'  => 'lightning_g3_three_column_unit_setting',
 				'settings' => 'lightning_g3_three_column_unit_options[2col_sub_sidebar_position]',
 				'type'     => 'select',
@@ -236,6 +381,27 @@ class Admin {
 				),
 			)
 		);
+
+		$wp_customize->add_setting(
+			'header-top-setting',
+			array(
+				'sanitize_callback' => 'sanitize_text_field',
+			)
+		);
+		$wp_customize->add_control(
+			new \VK_Custom_Html_Control(
+				$wp_customize,
+				'header-top-setting',
+				array(
+					'label'            => __( 'Header Top Setting', 'lightning-g3-three-column-unit' ),
+					'section'          => 'lightning_g3_three_column_unit_setting',
+					'type'             => 'text',
+					'custom_title_sub' => '',
+					'custom_html'      => '',
+				)
+			)
+		);
+		
 
 		// Narrow Window Discription.
 		$wp_customize->add_setting(
@@ -250,7 +416,7 @@ class Admin {
 		$wp_customize->add_control(
 			'lightning_g3_three_column_unit_options[narrow_window_description]',
 			array(
-				'label'    => __( 'Header Top Description Control on Narrow Window', 'lightning-g3-three-column-unit' ),
+				'label'    => __( 'Display Header Top Description on Narrow Window', 'lightning-g3-three-column-unit' ),
 				'section'  => 'lightning_g3_three_column_unit_setting',
 				'settings' => 'lightning_g3_three_column_unit_options[narrow_window_description]',
 				'type'     => 'select',
